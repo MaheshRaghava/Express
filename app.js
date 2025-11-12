f746948727c1b051e2fe6dfa5cd24822e7a87453
MaheshRaghava <raghava.raghava642@gmail.com>
2025-11-12 09:06:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/', (req, res) => {
+    const data = { title: 'Home Page', message: 'Welcome to EJS Example!' };
+    res.render('index', data); // Render the 'index.ejs' template
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
